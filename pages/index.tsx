c71c944253bcf54935427f4c38d341e61a08f1f9
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2022-09-04 13:00:13 +0200

    Added svg transition + added text + added image

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,6 +43,12 @@ const Home: NextPage = () => {
         "We offer 4 unique subjects for you and your child to choose from.",
     },
   ];
+  const carouselData = [
+    "/chalk.JPG",
+    "book-stack.JPG",
+    "children.JPG",
+    "pencil-circle.JPG",
+  ];
 
   // ELEMENTS: Elements are generated from data here: vvv
   const cardElements = (
@@ -58,38 +64,53 @@ const Home: NextPage = () => {
       ))}
     </div>
   );
+  const carouselElements = carouselData.map((item: string) => (
+    <Carousel.Item key={nanoid()}>
+      <picture className="relative">
+        <source srcSet={item} />
+        <img src={item} alt={item} className="h-96 w-full object-cover" />
+        <h1 className="absolute bottom-4 right-5 text-4xl font-itim font-medium text-white">
+          What&apos;s right for your child.
+        </h1>
+      </picture>
+    </Carousel.Item>
+  ));
 
   // CONTENT: Actually visible JSX goes here: vvv
   return (
     <main className="flex flex-col justify-center">
       <div className="">
-        <Carousel loop height={"150px"}>
-          {[
-            "/chalk.JPG",
-            "book-stack.JPG",
-            "children.JPG",
-            "pencil-circle.JPG",
-          ].map((item: string) => (
-            <Carousel.Item key={nanoid()}>
-              <picture className="relative">
-                <source srcSet={item} />
-                <img
-                  src={item}
-                  alt={item}
-                  className="h-96 w-full object-cover"
-                />
-                <h1 className="absolute bottom-4 right-5 text-4xl font-itim font-medium text-white">
-                  What&apos;s right for your child.
-                </h1>
-              </picture>
-            </Carousel.Item>
-          ))}
-        </Carousel>
+        <Carousel loop>{carouselElements}</Carousel>
       </div>
-      <h1 className="m-4 text-center text-5xl font-itim">
+      <h1 className="m-4 mb-2 text-center text-5xl font-itim">
         Prepare for your future!
       </h1>
-      {cardElements}
+      <p className="text-gray-800 text-center">
+        Out of a love for child development and a desperate need for an
+        alternative learning space,
+        <br />
+        Roxanne Edson established Marvel Planet Tutor Center in 2017.
+      </p>
+      <picture>
+        <img src="/waves/slate-100.svg" alt="" />
+      </picture>
+      <div className="grid grid-cols-2 pb-10 justify-between justify-items-center text-center font-itim font-light mt-0 bg-slate-100 px-10 text-neutral-800">
+        <picture>
+          <img
+            className="object-cover w-96 h-96 border-6 rounded-xl"
+            src="/pencils-left.JPG"
+            alt="Pencils"
+          />
+        </picture>
+        <p className="text-4xl text-ellipsis h-50">
+          Marvel Planet accommodates children with developmental delays,
+          children who have experienced trauma (such as bullying), learners with
+          mood or behavioural challenges as well as those who simply don&apos;t
+          fit into mainstream schooling - perhaps because they get bored easily
+          or are advanced in one particular subject but need more time in
+          another.
+        </p>
+      </div>
     </main>
   );
 };
