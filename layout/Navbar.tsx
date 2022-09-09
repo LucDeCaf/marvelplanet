b35bb147b6bb8d97305de7ae6d7b624b1978c8b7
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2022-09-09 13:26:27 +0200

    Added navbar menu and boilerplate for about / contact pages

--- layout/Navbar.tsx
+++ layout/Navbar.tsx
@@ -1,6 +1,25 @@
 import { HiMenu } from "react-icons/hi";
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
+
+const MenuItem = ({
+  children,
+  href,
+}: {
+  children: JSX.Element | string;
+  href: string;
+}): JSX.Element => {
+  return (
+    <li className="px-10 py-2 hover:bg-slate-300 active:bg-slate-400 w-screen">
+      <Link href={href}>
+        <a>
+          <li>{children}</li>
+        </a>
+      </Link>
+    </li>
+  );
+};
 
 const Navbar = (): JSX.Element => {
   const [showMenu, setShowMenu] = useState(false);
@@ -9,12 +28,19 @@ const Navbar = (): JSX.Element => {
     <nav className="sticky top-0 h-auto mb-2 bg-slate-100 shadow-md z-50">
       <div className="grid grid-cols-3 items-center py-3 px-6">
         <span className="relative w-32 h-32 justify-self-start">
-          <Image src="/logo.png" alt="Logo" layout="fill" width={432} height={387} />
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            layout="fill"
+            width={432}
+            height={387}
+          />
         </span>
         <div className="justify-self-center font-itim text-center tracking-tight">
           <h1 className="text-5xl lg:w-auto w-40">Marvel Planet</h1>
           <h2 className="lg:block hidden text-3xl">Tutor and Support Centre</h2>
         </div>
+        {/* Menu Button */}
         <div className="justify-self-end">
           <span
             className="text-5xl cursor-pointer"
@@ -24,10 +50,17 @@ const Navbar = (): JSX.Element => {
           </span>
         </div>
       </div>
+      {/* Menu */}
       {showMenu && (
-        <div className="px-10 flex text-xl font-medium">
-          <h1>Menu</h1>
-        </div>
+        <>
+          <div className="flex bg-slate-200 text-xl font-medium">
+            <ul>
+              <MenuItem href="/">Home</MenuItem>
+              <MenuItem href="/contact">Contact</MenuItem>
+              <MenuItem href="/about">About Us</MenuItem>
+            </ul>
+          </div>
+        </>
       )}
     </nav>
   );
---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Page: NextPage = () => {
+  return (
+    <main>
+      <h1>About Us Page</h1>
+    </main>
+  )
+}
+
+export default Page;
