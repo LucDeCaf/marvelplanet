5331594444c49e0aacf48841b94bc87b368f9100
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2022-09-12 19:13:47 +0200

    Added my contact info

--- layout/Footer.tsx
+++ layout/Footer.tsx
@@ -6,25 +6,31 @@ const Footer = (): JSX.Element => {
     <footer className="flex justify-between items-center bg-neutral-900 h-16 text-neutral-600 text-md p-4">
       <div>
         <p>
-          &#169; 2020 Marvel Planet Tutor Centre. All rights reserved.
-          Site by: LDC Studios
+          &#169; 2020 Marvel Planet Tutor Centre. All rights reserved. Site by:
+          LDC Studios
         </p>
       </div>
-      <div className="flex space-x-3 text-2xl">
-        <a
-          className="hover:text-white transition"
-          href="https://wa.me/+27662134916"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <BsWhatsapp />
-        </a>
-        <a className="hover:text-white transition" href="tel:+27662134916">
-          <BsTelephone />
-        </a>
-        <a className="hover:text-white transition" href="mailto:info@marvelplanet.co.za">
-          <HiMail />
-        </a>
+      <div className="flex space-x-3">
+        <p className="text-xl">Contact Us:</p>
+        <div className="flex text-2xl space-x-3">
+          <a
+            className="hover:text-white transition"
+            href="https://wa.me/+27660843239"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BsWhatsapp />
+          </a>
+          <a className="hover:text-white transition" href="tel:+27660843239">
+            <BsTelephone />
+          </a>
+          <a
+            className="hover:text-white transition"
+            href="mailto:ldecafmeyer@gmail.com"
+          >
+            <HiMail />
+          </a>
+        </div>
       </div>
     </footer>
   );
