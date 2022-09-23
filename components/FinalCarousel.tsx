9365812919a50955dcf3bed2633028379cdd8889
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2022-09-23 10:07:25 +0200

    Made having body text optional in cards

--- components/FinalCarousel.tsx
+++ components/FinalCarousel.tsx
@@ -3,20 +3,19 @@ import { nanoid } from "nanoid";
 
 interface Props {
   data: string[];
+  noText?: boolean;
 }
 
-const FinalCarousel = ({ data }: Props): JSX.Element => {
+const FinalCarousel = ({ data, noText }: Props): JSX.Element => {
   const carouselElements = data.map((item) => (
     <Carousel.Item key={nanoid()}>
       <picture className="relative">
-        <img
-          src={item}
-          alt={item}
-          className="h-96 w-full object-cover"
-        />
-        <span className="absolute bottom-4 right-5 text-4xl font-itim text-center font-medium text-white">
-          What&apos;s right for your child.
-        </span>
+        <img src={item} alt={item} className="h-96 w-full object-cover" />
+        {!noText && (
+          <span className="absolute bottom-4 right-5 text-4xl font-itim text-center font-medium text-white">
+            What&apos;s right for your child.
+          </span>
+        )}
       </picture>
     </Carousel.Item>
   ));
